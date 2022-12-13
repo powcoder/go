a94cea02ea47615979ed49cc99c463e34aeb98e5
powcoder <powcoder@163.com>
2022-12-13 15:26:42 +0800

    go

---
+++ SimpleAsset.go
@@ -0,0 +1,80 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/protos/peer"
+)
+
+// SimpleAsset 结构体
+type SimpleAsset struct {
+}
+
+// Init 实例化方法
+func (t *SimpleAsset) Init(stub shim.ChaincodeStubInterface) peer.Response {
+	return shim.Success(nil)
+}
+
+// Invoke 合约调用入口
+func (t *SimpleAsset) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+	fn, args := stub.GetFunctionAndParameters()
+
+	var result string
+	var err error
+	if fn == "set" {
+		result, err = set(stub, args)
+	} else {
+		result, err = get(stub, args)
+	}
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success([]byte(result))
+}
+
+// Set 上链操作
+func set(stub shim.ChaincodeStubInterface, args []string) (string, error) {
+	if len(args) != 2 {
+		return "", fmt.Errorf("Incorrect arguments. Expecting a key and a value")
+	}
+
+	err := stub.PutState(args[0], []byte(args[1]))
+	if err != nil {
+		return "", fmt.Errorf("Failed to set asset: %s", args[0])
+	}
+	return args[1], nil
+}
+
+// Get 查询操作
+func get(stub shim.ChaincodeStubInterface, args []string) (string, error) {
+	if len(args) != 1 {
+		return "", fmt.Errorf("Incorrect arguments. Expecting a key")
+	}
+
+	value, err := stub.GetState(args[0])
+	if err != nil {
+		return "", fmt.Errorf("Failed to get asset: %s with error: %s", args[0], err)
+	}
+	if value == nil {
+		return "", fmt.Errorf("Asset not found: %s", args[0])
+	}
+	return string(value), nil
+}
+
+// main 主函数
+func main() {
+	if err := shim.Start(new(SimpleAsset)); err != nil {
+		fmt.Printf("Error starting SimpleAsset chaincode: %s", err)
+	}
+}
